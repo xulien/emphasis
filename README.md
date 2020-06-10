48e3a0977abc6f4ca4ff971c65827370bc66bc5e
julien brunet <julien@brunet.io>
2020-06-10 07:23:40 +0200

    v0.3.0

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Emphasis
 
-Small function that normalizes a string which does not contain either for emphasis, spaces or special character.
+Small function that normalizes a string which does not contain either for emphasis, spaces or special character. Usefull for a clean url.
 
 ## Usage:
 
 ```javascript
 
-var norm = require('emphasis').normalize;
-var regex = require('emphasis').regex;
+const emphasis = require('emphasis')
 
-var naturalId = norm("Asperges blanches sauce printanière, Pâté de Pâques");
+const naturalId = emphasis("Asperges blanches sauce printanière, Pâté de Pâques")
 // asperges-blanches-sauce-printaniere-pate-de-paques
 
-var queryString = regex("Asperges blanches sauce printanière, Pâté de Pâques");
-// (a|à|â|ä)sp(e|é|è|ê|ë)rg(e|é|è|ê|ë)s\sbl(a|à|â|ä)nch(e|é|è|ê|ë)s\ss(a|à|â|ä)(u|ù|û|ü)c(e|é|è|ê|ë)\spr(i|î|ï)nt(a|à|â|ä)n(i|î|ï)(e|é|è|ê|ë)r(e|é|è|ê|ë),\sp(a|à|â|ä)t(e|é|è|ê|ë)\sd(e|é|è|ê|ë)\sp(a|à|â|ä)q(u|ù|û|ü)(e|é|è|ê|ë)s
-
 ```
+
+## Todo:
+
+* add tests...
--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-"use strict";
+'use strict'
 
-exports.regex = function(string) {
-        return string.toLowerCase()
-            .replace(/œ/g, "oe")
-            .replace(/ç/g, "(c|ç)")
-            .replace(/[uùûü]/g, "(u|ù|û|ü)")
-            .replace(/[iîï]/g, "(i|î|ï)")
-            .replace(/[aàâä]/g, "(a|à|â|ä)")
-            .replace(/[oôö]/g, "(o|ô|ö)")
-            .replace(/[eéèêë]/g, "(e|é|è|ê|ë)")
-            .replace(/ /g, "\\s");
-    };
-
-exports.normalize = function(string) {
-        return string.toLowerCase()
-            .replace(/[ùûü]/g, "u")
-            .replace(/[îï]/g, "i")
-            .replace(/[àâä]/g, "a")
-            .replace(/[ôö]/g, "o")
-            .replace(/œ/g, "oe")
-            .replace(/[éèêë]/g, "e")
-            .replace(/ç/g, "c")
-            .replace(/[’',!?&%@«»"\/()]/g, "-")
-            .replace(/ /g, "-")
-            .replace(/-+/g, "-");
-    };
+module.exports = uri => uri
+    .trim()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/['`’.#$]/g, '-')
+    .replace(/[.#$()«»?!\/\[\]^|]/g, '')
+    .replace(/œ/g, 'oe')
+    .toLowerCase()
+    .replace(/\s/g, '-')
+    .replace(/-{2,}/g, '')
